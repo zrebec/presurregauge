224f8bcb0e1a62f919248a920a15ebd67839f172
zrebec <zrebec@zrebec.sk>
2025-03-20 21:57:40 +0100

    feat: db add config

--- data/pressuregauge.sql
+++ data/pressuregauge.sql
@@ -1,8 +1,12 @@
 .mode table
 
-DROP TABLE IF EXISTS pressuregauge;
+CREATE TABLE IF NOT EXISTS config (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    key TEXT,
+    value TEXT
+);
 
-CREATE TABLE pressuregauge (
+CREATE TABLE IF NOT EXISTS pressuregauge (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     systolicpressure INT,
@@ -10,10 +14,15 @@ CREATE TABLE pressuregauge (
     heartrate INT DEFAULT NULL
 );
 
+/*
 INSERT INTO pressuregauge (date, systolicpressure, diastolicpressure, heartrate) VALUES
     ('2025-03-01 12:00', 120, 80, 60),
     ('2025-03-02 13:01', 130, 77, 60),
     ('2025-04-01 14:02', 96, 65, NULL);
+
+INSERT INTO config (key, value) VALUES ('version', '1.0');
+INSERT INTO config (key, value) VALUES ('seed', '');
+*/
 
 SELECT  strftime('%d. %m. %Y %H:%M:%S', date) 'Date', 
         systolicpressure || '/' || diastolicpressure 'Pressure',
