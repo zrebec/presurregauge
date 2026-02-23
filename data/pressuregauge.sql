246627604e3da63c07e289eaf30585a31fc27f98
zrebec <52221860+zrebec@users.noreply.github.com>
2026-02-23 07:13:42 +0100

    Added spo2 support to database

--- data/pressuregauge.sql
+++ data/pressuregauge.sql
@@ -1,6 +1,9 @@
 .mode table
 
-CREATE TABLE IF NOT EXISTS config (
+DROP TABLE IF EXISTS pressuregauge;
+DROP TABLE IF EXISTS config;
+    
+CREATE TABLE config (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     key TEXT,
     value TEXT
@@ -11,20 +14,23 @@ CREATE TABLE IF NOT EXISTS pressuregauge (
     date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     systolicpressure INT,
     diastolicpressure INT,
-    heartrate INT DEFAULT NULL
+    heartrate INT DEFAULT NULL,
+    spo2 INT DEFAULT NULL
 );
 
 /*
-INSERT INTO pressuregauge (date, systolicpressure, diastolicpressure, heartrate) VALUES
-    ('2025-03-01 12:00', 120, 80, 60),
-    ('2025-03-02 13:01', 130, 77, 60),
-    ('2025-04-01 14:02', 96, 65, NULL);
+INSERT INTO pressuregauge (date, systolicpressure, diastolicpressure, heartrate, spo2) VALUES
+    ('2025-03-01 12:00', 120, 80, 60, 92),
+    ('2025-03-02 13:01', 130, 77, 60, 89),
+    ('2025-04-01 14:02', 96, 65, NULL, 60),
+    ('2025-03-02 13:01', 133, 85, 53, 87),
+    ('2025-03-02 13:01', 111, 69, 85, 94);
 
-INSERT INTO config (key, value) VALUES ('version', '1.0');
+INSERT INTO config (key, value) VALUES ('version', '1.1');
 INSERT INTO config (key, value) VALUES ('seed', '');
 */
 
 SELECT  strftime('%d. %m. %Y %H:%M:%S', date) 'Date', 
         systolicpressure || '/' || diastolicpressure 'Pressure',
-        heartrate 'Heart Rate'
+        heartrate 'Heart Rate', spo2 'SpO2'
 FROM pressuregauge order by date desc;
